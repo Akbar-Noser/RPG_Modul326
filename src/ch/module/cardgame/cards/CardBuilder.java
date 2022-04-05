f352874f7c0d325da4246497d5e2d06605b14741
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2022-04-05 10:51:54 +0200

    Wrote Java doc for CardBuilder class

--- src/ch/module/cardgame/cards/CardBuilder.java
+++ src/ch/module/cardgame/cards/CardBuilder.java
@@ -8,16 +8,34 @@ public class CardBuilder {
 
     private int summonEnergyPoints;
 
+    /**
+     * Builder method for setting the health points of a Card when creating a Card object.
+     *
+     * @param healthPoints              int representing a card's health points
+     * @return                          the current cardBuilder
+     */
     public CardBuilder setHealthPoints(int healthPoints){
         this.healthPoints = healthPoints;
         return this;
     }
 
+    /**
+     * Builder method for setting the attack points of a Card when creating a Card object.
+     *
+     * @param attackPoints              int representing a card's attack points
+     * @return                          the current cardBuilder
+     */
     public CardBuilder setAttackPoints(int attackPoints){
         this.attackPoints = attackPoints;
         return this;
     }
 
+    /**
+     * Builder method for setting the summon energy of a Card when creating a Card object.
+     *
+     * @param summonEnergyPoints        int representing a card's summon energy
+     * @return                          the current cardBuilder
+     */
     public CardBuilder setSummonEnergyPoints(int summonEnergyPoints){
         this.summonEnergyPoints = summonEnergyPoints;
         return this;
