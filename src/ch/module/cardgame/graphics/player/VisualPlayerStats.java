3a920bfeeec7a54c3b314e8bcc9def838f44a590
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2022-05-09 20:47:15 +0200

    fixed display bug of playerstats

--- src/ch/module/cardgame/graphics/player/VisualPlayerStats.java
+++ src/ch/module/cardgame/graphics/player/VisualPlayerStats.java
@@ -9,14 +9,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class VisualPlayerStats extends JPanel {
+    private final JPanel container;
     private final Player owner;
     private final NumberIndicator health;
     private final NumberIndicator energy;
 
     public VisualPlayerStats(Player owner) {
+        this.container = new JPanel();
         this.owner = owner;
         health = new NumberIndicator(owner.getStats().getHealth() + "");
         energy = new NumberIndicator(owner.getStats().getEnergy() + "");
+        health.setForeground(ColorPalette.HEALTH);
+        energy.setForeground(ColorPalette.ENERGY);
+        adjustLayout();
         visualizeStats();
         setVisible(true);
     }
@@ -24,13 +29,26 @@ public class VisualPlayerStats extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(ImageLibrary.PLAYER_HEALTH_BAR.getImage(), 450,0, null);
-        g.drawImage(ImageLibrary.SUMMON_ENERGY_BAR.getImage(), 750,0, null);
+        g.drawImage(ImageLibrary.PLAYER_HEALTH_BAR.getImage(), 0,0, null);
+        g.drawImage(ImageLibrary.SUMMON_ENERGY_BAR.getImage(), 270,0, null);
+    }
+
+    private void adjustLayout() {
+        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+        container.add(Box.createHorizontalGlue());
+        container.add(this);
+        container.add(Box.createHorizontalGlue());
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(526, 62));
+        setMaximumSize(new Dimension(526, 128));
+        Dimension preferredSize = new Dimension(64, 80);
+        health.setPreferredSize(preferredSize);
+        health.setHorizontalAlignment(SwingConstants.CENTER);
+        energy.setPreferredSize(preferredSize);
+        energy.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
     private void visualizeStats() {
-        health.setForeground(ColorPalette.HEALTH);
-        energy.setForeground(ColorPalette.ENERGY);
         add(health, BorderLayout.WEST);
         add(energy, BorderLayout.EAST);
     }
